dbda1a6093e88015876cc4cf8253bd285d99cfd2
Alexander Staeding <alexanderstaeding@gmail.com>
2021-12-24 00:11:19 +0100

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+}
+
+tasks {
+    create<Jar>("graderJar") {
+        group = "build"
+        afterEvaluate {
+            archiveFileName.set("FOP-2022-H11-${project.version}.jar")
+            from(project(":grader").sourceSets.main.get().allSource)
+            from(project(":solution").sourceSets.main.get().allSource)
+        }
+    }
+}
+
+allprojects {
+    apply(plugin = "java")
+    version = "0.1.0-SNAPSHOT"
+    repositories {
+        mavenCentral()
+    }
+    java {
+        withSourcesJar()
+    }
+    tasks {
+        jar {
+            archiveFileName.set("${rootProject.name}-${project.name}.jar")
+        }
+        named<Jar>("sourcesJar") {
+            archiveFileName.set("${rootProject.name}-${project.name}-sources.jar")
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "FOP-2022-H10-Root"
+include("FOP-2022-H11-Student")
+include("grader")
+include("solution")
