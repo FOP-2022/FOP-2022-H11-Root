fc7ae0e697a72e27ca61339a14c6e04b4cd956a6
Daniel Mangold <mangold.daniel@protonmail.com>
2022-01-21 01:53:41 +0100

    Add class for pairs of characters and integers (H4)

---
+++ solution/src/main/java/h11/CharWithIndex.java
@@ -0,0 +1,25 @@
+package h11;
+
+import java.util.Objects;
+
+public class CharWithIndex {
+
+    private char theChar; // TODO: better name
+    private int index;
+
+    public CharWithIndex(Character character, Integer integer) {
+        Objects.requireNonNull(character);
+        Objects.requireNonNull(integer);
+
+        this.theChar = character;
+        this.index = integer;
+    }
+
+    public char getChar() {
+        return theChar;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+}
